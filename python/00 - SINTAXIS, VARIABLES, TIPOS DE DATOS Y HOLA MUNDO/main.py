f6240d53d8c5485e24b7a3e0bb11cb9c1125b106
RaeZlo <rramirezperazzolo@gmail.com>
2024-12-21 19:19:47 -0300

    Create main.py

---
+++ python/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/main.py
@@ -0,0 +1,26 @@
+# https://www.python.org/
+
+# Comentario en una linea
+
+"""
+Esta es una manera
+de
+comentar en varias lineas
+"""
+
+'''
+Otra forma de 
+comentar 
+en varias lineas
+'''
+
+# Tipos de datos primitivos
+CONSTANTE = "por convencion, Python no tiene constantes"
+string = "Hola, Python!"
+string = 'Otra forma de crear un str'
+int = 19
+float = 19.9
+complejo = 5j
+bool = True
+
+print("Hola, Python!")
